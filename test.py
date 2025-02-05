5edf767ac56c125f82425c6386f2decda99b07bb
hajeong67 <happykhj67@naver.com>
2025-02-05 14:53:13 +0900

    [add] metadata 확인용 코드

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-from django.db import connection
+import zipfile
 
-with connection.cursor() as cursor:
-    cursor.execute("DROP SCHEMA public CASCADE;")
-    cursor.execute("CREATE SCHEMA public;")
+whl_file_path = 'C:/Users/user/channels-4.2.0-py3-none-any.whl'
 
+with zipfile.ZipFile(whl_file_path, 'r') as z:
+    for filename in z.namelist():
+        if filename.endswith('METADATA'):
+            with z.open(filename) as metadata_file:
+                metadata_content = metadata_file.read().decode('utf-8')
+                print(metadata_content)
