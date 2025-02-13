20c78eeb6823b5724bc74e3e31f66735f4a06a2f
hajeong67 <happykhj67@naver.com>
2025-02-13 15:01:57 +0900

    [delete] 불필요한 주석 삭제

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -1,12 +1,26 @@
-# from django.core.management import BaseCommand
-# from users.models import users
-#
-# class Command(BaseCommand):
-#     help = 'creates'
-#     usename = 'ipinlabs_admin'
-#     email = 'hajeong@gmail.com'
-#     password = 'ipinlabs_ipinlabs'
-#
-#     def handle(self, *args, **options):
-#         try:
-#             user_models.User.objects.get(username=self.username)
+from django.core.management import BaseCommand
+from users.models import models as user_models
+
+class Command(BaseCommand):
+    help = 'This command creates superuser'
+    username = 'hj'
+    email = 'hajeong@gmail.com'
+    password = '1234'
+
+    def handle(self, *args, **options):
+        try:
+            user_models.User.objects.get(username=self.username)
+            self.stdout.write(self.style.SUCCESS(f"Superuser Exists"))
+
+        except user_models.User.DoesNotExist:
+            user_models.User.objects.create_superuser(
+                username=self.username,
+                email=self.email,
+                password=self.password,
+            )
+
+            self.stdout.write(self.style.SUCCESS(f"Superuser Created"))
+        except Exception as error:
+            self.stdout.write(
+                self.style.ERROR(f"Unknown Error caused: {error}")
+            )
